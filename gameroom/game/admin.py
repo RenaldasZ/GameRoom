f9e13b1eddf758497379a72c281a012c16b7e2d8
KarolisTam <130557721+KarolisTam@users.noreply.github.com>
2023-06-16 15:47:54 +0300

    Added hiscore template

--- gameroom/game/admin.py
+++ gameroom/game/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Player
 
 # Register your models here.
-admin.site.register(Player)
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'score')
+    list_filter = ('score', 'user')
+
+
+admin.site.register(Player, PlayerAdmin)
