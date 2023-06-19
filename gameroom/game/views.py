e9c3867ff0c93fd4fe27c5666274e258f13fc7a2
arnaszs <bolisas8@gmail.com>
2023-06-19 10:07:08 +0300

    kažką padariau

--- gameroom/game/views.py
+++ gameroom/game/views.py
@@ -4,12 +4,20 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from . models import Player
+from django.views.generic import View
+
 
 # Create your views here.
 def index(request):
     return render(request, 'gameweb/index.html')
 
 
-class HiScoreListView(generic.ListView):
-    model = Player
-    template_name = "gameweb/hiscore.html"
+class HiScoreListView(View):
+    def get(self, request):
+        top_players = Player.objects.order_by('-score')[:10]
+
+        context = {
+            'top_players': top_players
+        }
+
+        return render(request, 'gameweb/hiscore.html', context)
